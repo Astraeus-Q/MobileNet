c1b4c6b9bbaf4499103ee0ce53b59d96f58710c7
Astraeus-Q <1521050540@qq.com>
2024-03-22 23:50:28 +0800

    Test with lr=0.05, no lr_sche, 300 epochs.

--- run.sh
+++ run.sh
@@ -3,12 +3,12 @@ export CUBLAS_WORKSPACE_CONFIG=:16:8
 python main.py \
 --dataset_dir ./datasets \
 --batch_size 128 \
---epochs 15 \
---lr 0.01  --wd 0 \
+--epochs 300 \
+--lr 0.05  --wd 0 \
 --seed 0 \
---fig_name lr=0.01.png \
+--fig_name lr=0.05-lr_sche=const.png \
 --test
 # --mixup \
-# --lr_scheduler \
 # --wd 0.0005 \
+# --lr_scheduler \
 # --fig_name lr=0.05-lr_sche-wd=0.0005-mixup.png \
