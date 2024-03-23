ad0cee9a6b3c2e2452616dd57949c5d8090c8fb9
Astraeus-Q <1521050540@qq.com>
2024-03-24 02:20:35 +0800

    Test with lr=0.05, lr_sche=cos annealing, weight_decay=0.0001, 300 epochs.

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ python main.py \
 --dataset_dir ./datasets \
 --batch_size 128 \
 --epochs 300 \
---lr 0.05  --wd 0.0005 \
+--lr 0.05  --wd 0.0001 \
 --seed 0 \
 --lr_scheduler \
---fig_name lr=0.05-lr_sche=cos-wd=0.0005.png \
+--fig_name lr=0.05-lr_sche=cos-wd=0.0001.png \
 --test
 # --mixup \
 # --wd 0.0005 \
